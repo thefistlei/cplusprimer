e76b3c842e69bc7ceb4587652a3abc1f22261e03
LEIforever1 <thefist@126.com>
2020-12-13 15:38:40 +0800

    orderContainer

---
+++ cprimer/cprimer/orderContainer.h
@@ -0,0 +1,48 @@
+ 
+
+#ifndef ORDERCONTAINER_H 
+#define ORDERCONTAINER_H
+  
+#include <forward_list> 
+#include <iostream> 
+#include <fstream>
+#include <sstream>
+#include <istream>
+
+using std::string;  
+using std::vector;
+
+
+using std::cin; using std::cout; using std::endl;
+
+class orderContainer {
+public:
+	orderContainer(){}
+
+	virtual ~orderContainer() {}
+	 
+	void static test()
+	{
+		forward_list<int> vi = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+		auto prev = vi.before_begin();
+		auto curr = vi.begin();
+		while (curr != vi.end())
+		{
+			if (*curr % 2)
+			{
+				curr = vi.erase_after(prev);
+			}
+			else {
+				prev = curr;
+				curr++;
+			}
+		}
+
+		auto curr2 = vi.begin();
+		while (curr2 != vi.end()) {
+			cout << *curr2;
+			curr2++;
+		}
+	} 
+}; 
+#endif
